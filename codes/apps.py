0ce7136731bec1975d7d6496e319757fda2ab153
asd <asdzog@mail.ru>
2024-04-18 05:26:43 +0400

    remove codes app

--- codes/apps.py
+++
@@ -1,9 +0,0 @@
-from django.apps import AppConfig
-
-
-class CodesConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'codes'
-
-    def ready(self):
-        import codes.signals
