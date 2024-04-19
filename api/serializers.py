746d5a2ad29d453cfbbfe4282cfe9c6f1d14e4b8
asd <asdzog@mail.ru>
2024-04-19 08:03:05 +0400

    change serializers for API

--- api/serializers.py
+++ api/serializers.py
@@ -1,11 +1,27 @@
 from rest_framework import serializers
-
+from phonenumber_field.serializerfields import PhoneNumberField
 from users.models import User
 
 
-class UserSerializer(serializers.ModelSerializer):
+class SendCodeSerializer(serializers.Serializer):
+    phone_number = PhoneNumberField()
+
+
+class VerifyPhoneSerializer(serializers.Serializer):
+    phone_number = PhoneNumberField()
+    confirmation_code = serializers.CharField(max_length=4)
+
+
+class ProfileViewSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = User
-        fields = '__all__'
-        validators = []
+        fields = ('name', 'phone_number', 'invite_code', 'referrer')
+
+
+class ProfileUpdateSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = User
+        fields = ('name', 'phone_number', 'avatar', 'invite_code', 'referrer')
+        read_only_fields = ('invite_code', 'phone_number', )
