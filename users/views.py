209cd18da4f97d33a49e4a6620d3cd6a37e2172f
Aleksey <asdzog@mail.ru>
2024-04-19 15:17:10 +0400

    clean users views

--- users/views.py
+++ users/views.py
@@ -1,17 +1,4 @@
-import time
 
-from django.contrib.auth import login
-from django.contrib.auth.views import LogoutView
-from django.shortcuts import redirect, render
-from django.urls import reverse_lazy
-from django.views.generic import UpdateView
-from rest_framework import status
-from rest_framework.response import Response
-from rest_framework.views import APIView
-
-from api.serializers import AuthSerializer
-from users.models import User
-from users.managers import CustomUserManager
 
 
 
